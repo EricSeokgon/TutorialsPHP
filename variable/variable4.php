f4b82b65c5b6bd004d54b033c708f5d26895bfd8
hadesleee <leesk55@gmail.com>
2015-09-07 09:55:07 +0900

    변수 사용 한 방법

---
+++ variable/variable4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>variable1</title>
+</head>
+<body>
+<?php 
+$a = 100;
+$a = $a+10;
+print $a.'<br>';
+$a = $a /10;
+print $a.'<br>';
+$a = $a - 10;
+print $a.'<br>';
+$a = $a *10;
+print $a.'<br>';
+ ?>
+</body>
+</html>
