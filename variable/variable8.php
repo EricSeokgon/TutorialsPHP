dfc9779a8c059a9cbe48ce1c3a43ecb2a8f71165
hadesleee <leesk55@gmail.com>
2015-09-07 10:10:03 +0900

    상수 선언
    상수 선언

---
+++ variable/variable8.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>variable1</title>
+</head>
+<body>
+<?php 
+define('TITLE', 'PHP Tutorial');
+echo TITLE;
+define('TITLE', 'JAVA Turorial');
+ ?>
+</body>
+</html>
