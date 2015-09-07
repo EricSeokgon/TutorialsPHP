cbb5bcfd010e72acafff4028b294c9579f810ff0
hadesleee <leesk55@gmail.com>
2015-09-07 09:54:58 +0900

    변수 없이 사용

---
+++ variable/variable3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>variable1</title>
+</head>
+<body>
+<?php 
+echo (100+10).'<br>';
+echo ((100+10)/10).'<br>';
+echo (((100+10)/10)-10).'<br>';
+echo ((((100+10)/10)-10)*10).'<br>';
+ ?>
+</body>
+</html>
