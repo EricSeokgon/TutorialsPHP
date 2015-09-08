2cebf2fc4895029c94bccfadc2da9368c49e5f9a
haseslee <hadesleeq@gmail.com>
2015-09-08 11:24:37 +0900

    비교
    부정 연산자

---
+++ compare/compare2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>compare1</title>
+</head>
+<body>
+<?php 
+echo "1==2 : ";
+var_dump(1==2);
+echo "1==1 : ";
+var_dump(1==1);
+echo '<br>';
+echo '"one"=="one" : ';
+var_dump("one"=="one");
+echo '<br>';
+ ?>
+</body>
+</html>
