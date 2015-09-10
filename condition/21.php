7af551f2cf08093cc2ba1b647a81b1a241f7911e
haseslee <hadesleeq@gmail.com>
2015-09-10 10:42:26 +0900

    and or 복합 연산자

---
+++ condition/21.php
@@ -0,0 +1,8 @@
+<?php 
+if($_POST['id'] === 'sklee' or $_POST['id'] === 'k8805' or $_POST['id'] === 'hadeslee' and $_POST['password'] === '1111'){
+		echo 'right';		
+}else{
+	echo ' wrong';
+}
+
+?>
