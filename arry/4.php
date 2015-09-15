318255b42a801a6f9a51682ee34d39d0b7732628
haseslee <hadesleeq@gmail.com>
2015-09-15 09:50:42 +0900

    배열 기초

---
+++ arry/4.php
@@ -0,0 +1,20 @@
+<?php 
+function get_member1(){
+	return 'sklee';
+}
+
+echo get_member1().'<br>';
+
+function get_member2(){
+	return 'hadeslee';
+}
+
+echo get_member2().'<br>';
+
+function get_member3(){
+	return 'k8805';
+}
+
+echo get_member3().'<br>';
+
+ ?>
---
+++ arry/5.php
@@ -0,0 +1,13 @@
+<?php 
+function get_member(){
+	return ['sklee','hadeslee','k8805'];
+}
+
+//var_dump(get_member());
+
+//$tmp = get_member();
+//echo $tmp[1];
+
+echo get_member()[1];
+
+ ?>
